ff06af4e9b4204067741d2b0e4543c90774efde1
jonathan <jonathan876200@gmail.com>
2017-11-28 11:52:47 +0800

    commit

--- app/src/main/java/tom/com/atm/Login2Activity.java
+++ app/src/main/java/tom/com/atm/Login2Activity.java
@@ -1,7 +1,11 @@
 package tom.com.atm;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login2Activity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class Login2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
+    }
+
+    public void login(View view){
+        EditText edUserid = (EditText)findViewById(R.id.userid);
+        EditText edPasswd = (EditText)findViewById(R.id.passwd);
+        String userid = edUserid.getText().toString();
+        String passwd = edPasswd.getText().toString();
+        if("jonathan".equals(userid)&&"1234".equals(passwd)){
+            Toast.makeText(this,"登入成功",Toast.LENGTH_LONG).show();
+            finish();
+        }else{
+            new AlertDialog.Builder(this)
+                    .setTitle("登入")
+                    .setMessage("登入失敗")
+                    .setPositiveButton("OK",null)
+                    .show();
+        }
     }
 }
